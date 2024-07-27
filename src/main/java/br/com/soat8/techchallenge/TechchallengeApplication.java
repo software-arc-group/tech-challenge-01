8299300cfbd1b607f5ae7bb084324d9c57b1e243
Irlan Carlo <irlan.carlo@gmail.com>
2024-07-26 20:10:18 -0400

    configuração do ambiente e criação da estrutura de cliente

--- src/main/java/br/com/soat8/techchallenge/TechchallengeApplication.java
+++ src/main/java/br/com/soat8/techchallenge/TechchallengeApplication.java
@@ -2,8 +2,10 @@ package br.com.soat8.techchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"br.com.soat8.techchallenge.adapter.out.persistence.entity"})
 public class TechchallengeApplication {
 
 	public static void main(String[] args) {

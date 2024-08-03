1f48974af32b5d5cf0c02a1b08fadf720aba0fb2
washolv <w_olv@hotmail.com>
2024-08-03 12:35:56 -0300

    feat: adding QRCodeAdpater

--- src/main/java/br/com/soat8/techchallenge/adapter/in/controller/CustomerController.java
+++ src/main/java/br/com/soat8/techchallenge/adapter/in/controller/CustomerController.java
@@ -1,23 +1,24 @@
 package br.com.soat8.techchallenge.adapter.in.controller;
 
 import br.com.soat8.techchallenge.core.port.in.SaveCustomerUseCase;
+import br.com.soat8.techchallenge.core.service.QRCodeService;
 import br.com.soat8.techchallenge.domain.Customer;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/lanchonete")
 public class CustomerController {
 
     private final SaveCustomerUseCase saveCustomerUseCase;
+    private final QRCodeService qrCodeService;
 
-    public CustomerController(SaveCustomerUseCase saveCustomerUseCase) {
+    public CustomerController(SaveCustomerUseCase saveCustomerUseCase, QRCodeService qrCodeService) {
         this.saveCustomerUseCase = saveCustomerUseCase;
+        this.qrCodeService = qrCodeService;
     }
 
     @PostMapping("/customer")
--- src/main/java/br/com/soat8/techchallenge/adapter/in/controller/handler/ValidationExceptionHandler.java
+++ src/main/java/br/com/soat8/techchallenge/adapter/in/controller/handler/ValidationExceptionHandler.java
@@ -34,9 +34,5 @@ public class ValidationExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<String> handleAllUncaughtException(Exception ex) {
-        return new ResponseEntity<>("An unexpected error occurred. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+
 }

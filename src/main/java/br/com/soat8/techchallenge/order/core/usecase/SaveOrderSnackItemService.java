feac7c7629b34ba03e7967f7ae5e1d9258583e3e
visantanna <vinitsantanna@gmail.com>
2024-09-25 19:51:53 -0300

    mais alterações

--- src/main/java/br/com/soat8/techchallenge/order/core/usecase/SaveOrderSnackItemService.java
+++ src/main/java/br/com/soat8/techchallenge/order/core/usecase/SaveOrderSnackItemService.java
@@ -4,12 +4,18 @@ import br.com.soat8.techchallenge.order.core.entities.OrderSnackItem;
 import br.com.soat8.techchallenge.order.core.usecase.interfaces.SaveOrderSnackItemUseCase;
 import br.com.soat8.techchallenge.product.core.entities.Product;
 import br.com.soat8.techchallenge.product.utils.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
+@Service
 public class SaveOrderSnackItemService implements SaveOrderSnackItemUseCase {
 
+    @Autowired
     public ProductMapper productMapper;
+
+
 
     @Override
     public OrderSnackItem save(Product product, Integer qtd) {

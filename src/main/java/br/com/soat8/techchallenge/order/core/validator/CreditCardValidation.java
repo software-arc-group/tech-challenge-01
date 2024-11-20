f5c25cf21cbe51ff8bbb0aeca435b3e627ceb384
Washington Oliveira <w_olv@hotmail.com>
2024-11-20 14:39:31 -0300

    removendo arquivos desnecessarios

--- src/main/java/br/com/soat8/techchallenge/order/core/validator/CreditCardValidation.java
+++
@@ -1,8 +0,0 @@
-package br.com.soat8.techchallenge.order.core.validator;
-
-public class CreditCardValidation implements ValidationPaymentStrategy{
-    @Override
-    public void validate(Object payment) {
-
-    }
-}
--- src/main/java/br/com/soat8/techchallenge/order/core/validator/ValidationPaymentStrategy.java
+++
@@ -1,5 +0,0 @@
-package br.com.soat8.techchallenge.order.core.validator;
-
-public interface ValidationPaymentStrategy {
-    void validate(Object payment);
-}
